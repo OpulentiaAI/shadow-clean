aeacc0ea095a2dfefd1d859f1bfe8a99b6278f30
Opulent Intelligence <opulentintelligence@gmail.com>
2026-01-12 14:31:51 -0600

    Clean up accidentally committed .js/.d.ts files from convex folder

--- convex/testRunner.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Convex Module Test Suite
- * Tests all Convex-native functionality
- * Run with: CONVEX_URL=https://veracious-alligator-638.convex.cloud npx tsx convex/test-runner.ts
- */
-export {};
-//# sourceMappingURL=testRunner.d.ts.map
